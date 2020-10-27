3ddede2aa96cd47a22d57e841250be0010bb36ff
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2020-10-27 20:14:03 +0500

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function authenticated(Request $request, $user) {
+
+                 $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+                 $user->last_login = Carbon::now()->toDateTimeString();
+                 $user->api_token= $token;
+                 $user->save();
+    }
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Admin\User\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// 'prefix' => 'admin',
+Route::group(['middleware' => 'api'], function()
+{
+    //All the routes that belongs to the group goes here
+    Route::get('/users',[UserController::class,'index']);
+});
