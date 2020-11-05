a082538f6d1c8789e27108c730e713b20d3ef3f5
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2020-11-05 19:45:39 +0500

    new  dates

--- resources/js/components/actions/DateFilterComponent.vue
+++ resources/js/components/actions/DateFilterComponent.vue
@@ -1,12 +1,19 @@
 <template>
-   <a class="dropdown-item" href="#" v-show="date_show">
-       <i class="mdi mdi-calendar-text mr-2 text-success">
+   <a class="dropdown-item btn btn-primary mt-1 model-animation-btn" role="button" v-show="date_show">
+       <i class="mdi mdi-calendar-text mr-2 text-success" @click="openDateModal">
     </i>{{label_name}}</a>
+
+
 </template>
 
 <script>
 export default {
-props: ['label_name','date_show']
+props: ['label_name','date_show'],
+methods:{
+    openDateModal(){
+        alert("hello");
+    },
+},
 }
 </script>
 
